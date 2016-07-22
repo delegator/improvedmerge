978ecccae1e2f0241bb1ece9114de51eedbfd23a
Tom Richards <tom.r@delegator.com>
2016-07-22 16:45:30 -0400

    Require the same minifiers used by Magento 2

--- app/code/local/Delegator/Improvedmerge/Model/Design/Package.php
+++ app/code/local/Delegator/Improvedmerge/Model/Design/Package.php
@@ -15,6 +15,90 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
         return $acc;
     }
 
+    public function _mergeFiles(array $srcFiles, $targetFile = false,
+        $mustMerge = false, $beforeMergeCallback = null, $extensionsFilter = [])
+    {
+        try {
+            // check whether merger is required
+           $shouldMerge = $mustMerge || !$targetFile;
+            if (!$shouldMerge) {
+                if (!file_exists($targetFile)) {
+                    $shouldMerge = true;
+                } else {
+                    $targetMtime = filemtime($targetFile);
+                    foreach ($srcFiles as $file) {
+                        if (!file_exists($file) || @filemtime($file) > $targetMtime) {
+                            $shouldMerge = true;
+                            break;
+                        }
+                    }
+                }
+            }
+           // merge contents into the file
+           if ($shouldMerge) {
+               if ($targetFile && !is_writeable(dirname($targetFile))) {
+                   // no translation intentionally
+                   throw new Exception(sprintf('Path %s is not writeable.', dirname($targetFile)));
+               }
+               // filter by extensions
+               if ($extensionsFilter) {
+                   if (!is_array($extensionsFilter)) {
+                       $extensionsFilter = array($extensionsFilter);
+                   }
+                   if (!empty($srcFiles)) {
+                       foreach ($srcFiles as $key => $file) {
+                           $fileExt = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+                           if (!in_array($fileExt, $extensionsFilter)) {
+                               unset($srcFiles[$key]);
+                           }
+                       }
+                   }
+               }
+               if (empty($srcFiles)) {
+                   // no translation intentionally
+                   throw new Exception('No files to compile.');
+               }
+               $data = '';
+               foreach ($srcFiles as $file) {
+                   if (!file_exists($file)) {
+                       continue;
+                   }
+                   $contents = file_get_contents($file)."\n";
+                   if ($beforeMergeCallback && is_callable($beforeMergeCallback)) {
+                       $contents = call_user_func($beforeMergeCallback, $file, $contents);
+                   }
+                   $data .= $contents;
+               }
+               if (!$data) {
+                   // no translation intentionally
+                   throw new Exception(sprintf("No content found in files:\n%s", implode("\n", $srcFiles)));
+               }
+
+               if ($extensionsFilter === ['js']) {
+                   Mage::log('minifying js!');
+                   $data = \JShrink\Minifier::minify($data, ['flaggedComments' => false]);
+               } elseif ($extensionsFilter === ['css']) {
+                   Mage::log('minifying css!');
+                   $compressor = new CSSmin();
+                   $data = $compressor->run($data);
+               }
+
+               if ($targetFile) {
+                   file_put_contents($targetFile, $data, LOCK_EX);
+                   file_put_contents($targetFile.'.gz', gzencode($data, 9), LOCK_EX);
+               } else {
+                   return $data; // no need to write to file, just return data
+               }
+           }
+
+            return true; // no need in merger or merged into file successfully
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+
+        return false;
+    }
+
     /**
      * @ignore
      */
@@ -27,8 +111,8 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
         // Determine timestamp of most recently modified file
         $latestTime = array_reduce($files, [$this, 'filetimeReduce'], 0);
         $filesList = implode(',', $files);
-        $hash = hash('sha256', $filesList . $latestTime);
-        $targetFilename = $hash . '.' . $extensions;
+        $hash = hash('sha256', $filesList.$latestTime);
+        $targetFilename = $hash.'.'.$extensions;
 
         // Initialize merge directory
         $targetDir = $this->_initMergerDir($mergeDir);
@@ -39,15 +123,16 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
         // Try to merge files
         $mergeFilesResult = $this->_mergeFiles(
             $files,
-            $targetDir . DS . $targetFilename,
+            $targetDir.DS.$targetFilename,
             false,
             $callbacks,
             $extensions
         );
 
         if ($mergeFilesResult) {
-            return $baseMediaUrl . $mergeDir . '/' . $targetFilename;
+            return $baseMediaUrl.$mergeDir.'/'.$targetFilename;
         }
+
         return '';
     }
 
@@ -55,8 +140,9 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
      * Merges the contents of the provided JavaScript files.
      *
      * @param array $files List of JavaScript files (paths) to be merged.
+     *
      * @return string When successful, returns the URL of the merged file.
-     * Otherwise, returns an empty string.
+     *                Otherwise, returns an empty string.
      */
     public function getMergedJsUrl($files)
     {
@@ -67,8 +153,9 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
      * Merges the contents of the provided CSS files.
      *
      * @param array $files List of CSS files (paths) to be merged.
+     *
      * @return string When successful, returns the URL of the merged file.
-     * Otherwise, returns an empty string.
+     *                Otherwise, returns an empty string.
      */
     public function getMergedCssUrl($files)
     {
