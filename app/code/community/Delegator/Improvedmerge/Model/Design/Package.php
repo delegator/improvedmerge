delegator/improvedmerge

commit e1880eef877f57d2f32a541722ea5137cbe6a020

Fix lint errors, set severity

app/code/community/Delegator/Improvedmerge/Model/Design/Package.php
@@ -18,9 +18,13 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
     /**
      * @ignore
      */
-    public function _mergeFiles(array $srcFiles, $targetFile = false,
-                                $mustMerge = false, $beforeMergeCallback = null, $extensionsFilter = [])
-    {
+    public function _mergeFiles(
+        array $srcFiles,
+        $targetFile = false,
+        $mustMerge = false,
+        $beforeMergeCallback = null,
+        $extensionsFilter = array()
+    ) {
         try {
             // check whether merger is required
             $shouldMerge = $mustMerge || !$targetFile;
@@ -37,17 +41,20 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
                     }
                 }
             }
+
             // merge contents into the file
             if ($shouldMerge) {
                 if ($targetFile && !is_writeable(dirname($targetFile))) {
                     // no translation intentionally
                     throw new Exception(sprintf('Path %s is not writeable.', dirname($targetFile)));
                 }
+
                 // filter by extensions
                 if ($extensionsFilter) {
                     if (!is_array($extensionsFilter)) {
                         $extensionsFilter = array($extensionsFilter);
                     }
+
                     if (!empty($srcFiles)) {
                         foreach ($srcFiles as $key => $file) {
                             $fileExt = strtolower(pathinfo($file, PATHINFO_EXTENSION));
@@ -57,35 +64,40 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
                         }
                     }
                 }
+
                 if (empty($srcFiles)) {
                     // no translation intentionally
                     throw new Exception('No files to compile.');
                 }
+
                 $data = '';
                 foreach ($srcFiles as $file) {
                     if (!file_exists($file)) {
                         continue;
                     }
+
                     $contents = file_get_contents($file) . "\n";
                     if ($beforeMergeCallback && is_callable($beforeMergeCallback)) {
                         $contents = call_user_func($beforeMergeCallback, $file, $contents);
                     }
+
                     $data .= $contents;
                 }
+
                 if (!$data) {
                     // no translation intentionally
                     throw new Exception(sprintf("No content found in files:\n%s", implode("\n", $srcFiles)));
                 }
 
-                if ($extensionsFilter === ['js']) {
+                if ($extensionsFilter === array('js')) {
                     $bench = new Ubench;
                     $bench->start();
-                    $data = \JShrink\Minifier::minify($data, ['flaggedComments' => false]);
+                    $data = \JShrink\Minifier::minify($data, array('flaggedComments' => false));
                     $bench->end();
                     if (getenv('DG_IMPROVEDMERGE_DEBUG') !== false) {
                         Mage::log('Minified JS in ' . $bench->getTime());
                     }
-                } elseif ($extensionsFilter === ['css']) {
+                } elseif ($extensionsFilter === array('js')) {
                     $bench = new Ubench;
                     $bench->start();
                     $compressor = new CSSmin();
@@ -122,7 +134,7 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
         $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
 
         // Determine timestamp of most recently modified file
-        $latestTime = array_reduce($files, [$this, 'filetimeReduce'], 0);
+        $latestTime = array_reduce($files, array($this, 'filetimeReduce'), 0);
         $filesList = implode(',', $files);
         $hash = hash('sha256', $filesList . $latestTime);
         $targetFilename = $hash . '.' . $extensions;
@@ -176,7 +188,7 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
             $files,
             Mage::app()->getRequest()->isSecure() ? 'css_secure' : 'css',
             'css',
-            [$this, 'beforeMergeCss']
+            array($this, 'beforeMergeCss')
         );
     }
 }

app/code/community/Delegator/Improvedmerge/Block/Html/Head.php
@@ -7,9 +7,10 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
         // separate items by types
         $lines  = array();
         foreach ($this->_data['items'] as $item) {
-            if (!is_null($item['cond']) && !$this->getData($item['cond']) || !isset($item['name'])) {
+            if (!(null === $item['cond']) && !$this->getData($item['cond']) || !isset($item['name'])) {
                 continue;
             }
+
             $if     = !empty($item['if']) ? $item['if'] : '';
             $params = !empty($item['params']) ? $item['params'] : '';
             switch ($item['type']) {
@@ -24,6 +25,7 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
                     break;
             }
         }
+
         // prepare HTML
         $shouldMergeJs = Mage::getStoreConfigFlag('dev/js/merge_files');
         $shouldMergeCss = Mage::getStoreConfigFlag('dev/css/merge_css_files');
@@ -32,6 +34,7 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
             if (empty($items)) {
                 continue;
             }
+
             if (!empty($if)) {
                 // open !IE conditional using raw value
                 if (strpos($if, "><!-->") !== false) {
@@ -40,14 +43,17 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
                     $html .= '<!--[if '.$if.']>' . "\n";
                 }
             }
+
             // static and skin css
-            $html .= $this->_prepareStaticAndSkinElements('<link rel="stylesheet" type="text/css" href="%s"%s />'."\n",
+            $html .= $this->_prepareStaticAndSkinElements(
+                '<link rel="stylesheet" type="text/css" href="%s"%s />'."\n",
                 empty($items['js_css']) ? array() : $items['js_css'],
                 empty($items['skin_css']) ? array() : $items['skin_css'],
                 $shouldMergeCss ? array(Mage::getDesign(), 'getMergedCssUrl') : null
             );
             // static and skin javascripts
-            $html .= $this->_prepareStaticAndSkinElements('<script type="text/javascript" src="%s" crossorigin="anonymous"%s></script>' . "\n",
+            $html .= $this->_prepareStaticAndSkinElements(
+                '<script type="text/javascript" src="%s" crossorigin="anonymous"%s></script>' . "\n",
                 empty($items['js']) ? array() : $items['js'],
                 empty($items['skin_js']) ? array() : $items['skin_js'],
                 $shouldMergeJs ? array(Mage::getDesign(), 'getMergedJsUrl') : null
@@ -56,6 +62,7 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
             if (!empty($items['other'])) {
                 $html .= $this->_prepareOtherHtmlHeadElements($items['other']) . "\n";
             }
+
             if (!empty($if)) {
                 // close !IE conditional comments correctly
                 if (strpos($if, "><!-->") !== false) {
@@ -65,6 +72,7 @@ class Delegator_Improvedmerge_Block_Html_Head extends Mage_Page_Block_Html_Head
                 }
             }
         }
+
         return $html;
     }
 }

phpcs.xml
@@ -8,6 +8,9 @@
     <!-- Exclude these directories from linting -->
     <exclude-pattern>vendor/*</exclude-pattern>
 
+    <!-- Only check for high severity issues -->
+    <arg name="severity" value="10" />
+
     <!-- Show colors in console -->
     <arg value="-colors" />
 
@@ -18,8 +21,5 @@
     <arg name="extensions" value="php,phtml" />
 
     <!-- Use MEQP1 as a base -->
-    <rule ref="MEQP1">
-        <!-- Exclude some rules which are obviously mistakes -->
-        <exclude name="PSR1.Classes.ClassDeclaration.MissingNamespace" />
-    </rule>
+    <rule ref="MEQP1" />
 </ruleset>

commit 83864a26e58d46d9c36b19e095a1910b6417bd76

Fix css minification error
This error was introduced in e1880eef877f57d2f32a541722ea5137cbe6a020

app/code/community/Delegator/Improvedmerge/Model/Design/Package.php
@@ -97,7 +97,7 @@ class Delegator_Improvedmerge_Model_Design_Package extends Mage_Core_Model_Desig
                     if (getenv('DG_IMPROVEDMERGE_DEBUG') !== false) {
                         Mage::log('Minified JS in ' . $bench->getTime());
                     }
-                } elseif ($extensionsFilter === array('js')) {
+                } elseif ($extensionsFilter === array('css')) {
                     $bench = new Ubench;
                     $bench->start();
                     $compressor = new CSSmin();